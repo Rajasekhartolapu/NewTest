bd099ea4f196075fc293769fe1374e1e47cd4a36
Raj Sekhar <raj.sekhar@admins-mac-mini.kelltonggn.com>
2018-06-22 18:13:17 +0530

    Initial Commit

---
+++ VersionControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VersionControl
+//
+//  Created by Raj Sekhar on 6/22/18.
+//  Copyright © 2018 Raj Sekhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
